b8b8cbe819211c3bfe2fbcf4bbe71fd688557a13
kenkenpa198 <kenkenpa198@gmail.com>
2025-05-27 23:56:40 +0900

    Add SQL scripts to create APPUSER and EXAMPLES table with verification

---
+++ script/setup/01_create_appuser.sql
@@ -0,0 +1,12 @@
+-- Create APPUSER user
+CREATE USER APPUSER IDENTIFIED BY appuser_password;
+
+-- Grant necessary privileges to APPUSER
+GRANT CONNECT,
+RESOURCE TO APPUSER;
+
+-- Assign quota on USERS tablespace
+ALTER USER APPUSER QUOTA UNLIMITED ON USERS;
+
+-- Set default tablespace
+ALTER USER APPUSER DEFAULT TABLESPACE USERS;
